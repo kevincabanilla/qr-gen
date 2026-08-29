c3169a2e0ba05c1f1daef6b1a30f3e6b9047ab55
Kevin Cabanilla <kevincabanilla.dev@gmail.com>
2026-08-29 16:10:22 +0800

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,4 @@
-export const SIZE_OPTIONS = [32, 64, 128, 512, 1080];
+export const MAX_URL_LENGTH = 2000;
+export const SIZE_OPTIONS = [32, 64, 128, 256, 512, 1080];
+export const IMG_FILE_TYPES = ["image/png", "image/jpeg", "image/svg+xml"];
+export const MAX_FILE_SIZE = 10000;
