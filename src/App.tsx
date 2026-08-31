c7dbb95b0071846d2722f37731733eec49bde53c
Kevin Cabanilla <kevincabanilla.dev@gmail.com>
2026-08-31 13:07:59 +0800

    Add QRForm to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 import "./App.css";
+import { QRCodeCanvas } from "./components/QRCodeCanvas";
+import QRForm from "./components/QRForm";
+import { DownloadButton } from "./components/DownloadButton";
+import type { QRFormData } from "./hooks/useQRForm";
 
 function App() {
-  return <main></main>;
+  const [qrValue, setQrValue] = useState("");
+  const [size, setSize] = useState(0);
+  const [logo, setLogo] = useState<File | null>(null);
+  const [circularLogo, setCircularLogo] = useState(false);
+
+  const [showQR, setShowQR] = useState(false);
+
+  const generateQR = (data: QRFormData, logo: File | null) => {
+    setQrValue(data.url);
+    setSize(data.size);
+    setLogo(logo);
+    setCircularLogo(data.circularLogo ?? false);
+    setShowQR(true);
+  };
+
+  return (
+    <main>
+      <div className="h-screen w-screen flex flex-col md:flex-row justify-center items-center gap-4">
+        <QRForm onGenerate={generateQR} />
+
+      </div>
+    </main>
+  );
 }
 
 export default App;
